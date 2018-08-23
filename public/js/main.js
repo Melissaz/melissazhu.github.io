0b4dcc70eaabc56c02951ce41a85c150e73704ef
melissa <melissazhu223@gmail.com>
2018-08-23 16:23:33 +1000

    contact page partly finished

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+// google map
+function myMap() {
+var mapProp= {
+center:new google.maps.LatLng(-27.469770,153.025131),
+zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
