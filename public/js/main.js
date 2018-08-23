8a7a8fe535c7a1069ec1652f24077dc77d040ed2
melissa <melissazhu223@gmail.com>
2018-08-24 09:46:36 +1000

    finished

--- public/js/main.js
+++ public/js/main.js
@@ -7,35 +7,44 @@ zoom:5,
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 
-// google recaptcha
-iframe: {
-    markup: '<div class="mfp-iframe-scaler">'+
-    '<div class="mfp-close"></div>'+
-    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-    '<div class="mfp-title mfp-bottom-iframe-title"></div>'+
-    '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
 
-    patterns: {
-        youtube: {
-            index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
 
-            id: null, // String that splits URL in a two parts, second part should be %id%
-            // Or null - full URL will be returned
-            // Or a function that should return %id%, for example:
-            // id: function(url) { return 'parsed id'; }
+(function ($) {
+    'use strict';
 
-            src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
-        },
-        vimeo: {
-            index: 'vimeo.com/',
-            id: '/',
-            src: '//player.vimeo.com/video/%id%?autoplay=1'
-        },
-        gmaps: {
-            index: '//maps.google.',
-            src: '%id%&output=embed'
-        }
-    },
+    var form = $('.contact__form'),
+        message = $('.contact__msg'),
+        form_data;
 
-    srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-},
+    // Success function
+    function done_func(response) {
+        message.fadeIn()
+        message.html(response);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 5000);
+        
+        form.find('input:not([type="submit"]), textarea').val('');
+    }
+
+    // fail function
+    function fail_func(data) {
+        message.fadeIn()
+        message.html(data.responseText);
+        setTimeout(function () {
+            message.fadeOut(5000);
+        }, 5000);
+    }
+    
+    form.submit(function (e) {
+        e.preventDefault();
+        form_data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: form_data
+        })
+        .done(done_func)
+        .fail(fail_func);
+    });
+})(jQuery);
